3c60c242a99b99fbefb1ad5626490c605eb6e8e3
abhibhandari1 <abhishek.bhandari@kellogg.northwestern.edu>
2022-05-31 03:33:35 +0000

    -

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,11 +5,17 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new
-    @user["first_name"] = params["user"]["first_name"]
-    @user["last_name"] = params["user"]["last_name"]
+    @user["username"] = params["user"]["username"]
     @user["email"] = params["user"]["email"]
-    @user["password"] = params["user"]["password"]
-    @user.save
-    redirect_to "/"
+    @user["password"] = BCrypt::Password.create(params["user"]["password"])
+        
+    if User.exists?("email" => @user["email"])
+      flash["notice"] = "User ID already exists"
+    else
+      flash["notice"] = "Welcome to Tacostagram"
+      @user.save      
+    end
+   
+    redirect_to "/sessions/new"
   end
 end
